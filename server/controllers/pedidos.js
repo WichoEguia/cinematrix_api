899ccac1eab390aa25bdc4bb9c3788a95c97b7b4
Luis Eguia <jlewicho@gmail.com>
2018-09-21 01:03:15 -0500

    Obteniendo tipo y cantidad de boletos en API

--- server/controllers/pedidos.js
+++ server/controllers/pedidos.js
@@ -7,33 +7,52 @@ const { verificaToken, verificaAdminRole } = require('../middlewares/autenticaci
 const app = express();
 
 app.post('/pedidos/crear', verificaToken, (req, res) => {
-    let body = req.body;
+    getBoletos().then((res_boletos) => {
+        let body = req.body;
 
-    let pedido = new Pedido({
-        'hora_llegada': body.hora_llegada,
-        'boletos': body.boletos,
-        'usuario': req.usuario._id,
-        'funcion': body.funcion
-    });
+        // Obtener boletos
+        let boletos = [];
+        body.boletos.forEach(b => {
+            boleto = b.split(',');
+            for (let i = 0; i < boleto[1]; i++) {
+                var result = res_boletos.filter((obj) => {
+                    return obj.tipo === boleto[0]
+                });
+                boletos.push(result[0]._id);
+            }
+        });
 
-    pedido.save((err, pedidoDB) => {
-        if (err) {
-            return res.status(500).json({
-                ok: false,
-                err
+        let pedido = new Pedido({
+            'hora_llegada': body.hora_llegada,
+            'boletos': boletos,
+            'usuario': req.usuario._id,
+            'funcion': body.funcion
+        });
+
+        pedido.save((err, pedidoDB) => {
+            if (err) {
+                return res.status(500).json({
+                    ok: false,
+                    err
+                });
+            }
+
+            if (!pedidoDB) {
+                return res.status(500).json({
+                    ok: false,
+                    err: { message: 'No se pudo crear el pedido' }
+                });
+            }
+
+            res.json({
+                ok: true,
+                pedido: pedidoDB
             });
-        }
-
-        if (!pedidoDB) {
-            return res.status(500).json({
-                ok: false,
-                err: { message: 'No se pudo crear el pedido' }
-            });
-        }
-
-        res.json({
-            ok: true,
-            pedido: pedidoDB
+        });
+    }, (err) => {
+        return res.status(500).json({
+            ok: false,
+            err
         });
     });
 });
@@ -60,5 +79,20 @@ app.get('/pedidos/ver', verificaToken, (req, res) => {
         });
     });
 });
+
+let getBoletos = () => {
+    return new Promise((resolve, reject) => {
+        Boleto.find().exec((err, boletos) => {
+            if (err) {
+                reject({
+                    ok: false,
+                    err
+                });
+            }
+
+            resolve(boletos);
+        });
+    });
+}
 
 module.exports = app;
